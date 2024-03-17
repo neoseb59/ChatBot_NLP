027add084191249616e99af346e8f22617084a40
Thaïs <101876673+thaiscodafond@users.noreply.github.com>
2024-03-17 14:19:53 +0100

    feat : loader

---
+++ src/xmlloader.py
@@ -0,0 +1,11 @@
+class XMLloader:
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def load(self):
+        with open(self.data_path, 'r') as f:
+            return f.read()
+
+
+if __name__ == '__main__':
+    pass
