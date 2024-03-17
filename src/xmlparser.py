742703a80e7a5e67ad271504da2e8e715daa12af
Thaïs <101876673+thaiscodafond@users.noreply.github.com>
2024-03-17 14:19:37 +0100

    fix : parser duplicate

--- src/xmlparser.py
+++ src/xmlparser.py
@@ -1,42 +1,63 @@
 # coding: utf-8
-
 from bs4 import BeautifulSoup
+import json
 import re
-import os
-
+## A AMELIORER 
 class XMLParser:
-    def __init__(self, xml):
+    def __init__(self, xml, output_path=None):
         self.xml = xml
+        self.parsed_data = None
+        self.output_path = output_path
 
     def parse(self):
         soup = BeautifulSoup(self.xml, 'xml')
         questions_and_answers = []
+
         chapters = soup.find_all('Chapitre')
         
         for chapter in chapters:
             question = chapter.find('Titre').find('Paragraphe').get_text().replace('\xa0', ' ')
-            
             if question.endswith('?'):
                 answer_paragraphs = chapter.find_all('Paragraphe')[1:]
                 answer = ' '.join([p.get_text().replace('\xa0', ' ') for p in answer_paragraphs])
 
                 if any(p.get_text().strip().endswith('?') for p in answer_paragraphs):
-                    questions_and_answers.append((question, ''))
+                    questions_and_answers.append({'question': question, 'response': ''})
 
                     for p in answer_paragraphs:
                         if p.get_text().strip().endswith('?'):
                             questions = re.findall(r'\.\s*\w+\s*\?', p.get_text())
                             if questions:
                                 for found_question in questions:
-                                    questions_and_answers.append((found_question, p.get_text().replace('\xa0', ' ')))
+                                    questions_and_answers.append({'question': found_question, 'response': p.get_text().replace('\xa0', ' ')})
                             else:
-                                print("No questions found in paragraph:", p.get_text())
+                                pass
                         else:
-                            questions_and_answers[-1] = (questions_and_answers[-1][0], questions_and_answers[-1][1] + ' ' + p.get_text().replace('\xa0', ' '))
+                            questions_and_answers[-1]['response'] += ' ' + p.get_text().replace('\xa0', ' ')
                 else:
-                    questions_and_answers.append((question, answer))
+                    questions_and_answers.append({'question': question, 'response': answer})
                     
-        return questions_and_answers
+        self.parsed_data = questions_and_answers
+        self.unique_data()
+
+        return self.parsed_data
+
+
+    def unique_data(self):
+        unique_pairs = set()
+        unique_data = []
+
+        for pair in self.parsed_data:
+            pair_tuple = (pair['question'], pair['response'])  # Convert dictionary to tuple
+            if pair_tuple not in unique_pairs:
+                unique_data.append(pair)
+                unique_pairs.add(pair_tuple)
+        self.parsed_data = unique_data
+
+
+    def output_json(self):
+        with open(self.output_path, 'w') as json_file:
+            json.dump(self.parsed_data, json_file, indent=4, ensure_ascii=False)
 
 
     def get_xml(self):
@@ -45,15 +66,8 @@ class XMLParser:
     def set_xml(self, xml):
         self.xml = xml
     
-    def import_xml(self, file_path):
-        with open(file_path, 'r') as file:
-            return file.read()
 
 if __name__ == '__main__':
-    xml_parser = XMLParser('')
-    xml_doc = xml_parser.import_xml('../data/part/F32485.xml')
-    xml_parser.set_xml(xml_doc)
-    parsed_questions_and_answers = xml_parser.parse()
-    print(parsed_questions_and_answers)
+    pass
 
 
