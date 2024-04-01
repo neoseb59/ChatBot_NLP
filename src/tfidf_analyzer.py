Fixed error in TF-IDF formula

src/tfidf_analyzer.py
@@ -1,27 +1,36 @@
 import json
 import numpy as np
 from pathlib import Path
-from tqdm import tqdm
 from token_tagger import TokenTagger
 from termhood_analyzer import TermhoodAnalyzer
 
 class TfidfAnalyzer:
 
-    def __init__(self, corpus: list[dict[str, str]], terms: set[str], document_frequencies: dict[str, int]):
+    def __init__(self, corpus: list[dict[str, str]], terms: set[str]):
         self.corpus = corpus
         self.terms = terms
-        self.document_frequencies = document_frequencies
+        self.document_frequencies = self.find_document_frequencies()
+
+    def find_document_frequencies(self):
+        document_frequencies = {}
+        for term in self.terms:
+            document_frequency = 0
+            for response in [pair['response'] for pair in self.corpus]:
+                if term in response:
+                    document_frequency += 1
+            document_frequencies[term] = document_frequency
+        return document_frequencies
 
     def run(self) -> dict[str, dict[str, float]]:
-        print("Computing TF-IDF scores...")
+        print("[TF-IDF Analyzer] Computing TF-IDF scores...")
         all_feature_scores = {}
-        for response in tqdm([pair['response'] for pair in self.corpus], total=len(self.corpus)):
+        for response in [pair['response'] for pair in self.corpus]:
             feature_scores = {}
             terms_in_response = 0
             for any_term in self.terms:
                 terms_in_response += response.count(any_term)
             for term in self.terms:
-                if terms_in_response > 0:
+                if self.document_frequencies[term] > 0 and terms_in_response > 0:
                     term_frequency = response.count(term) / terms_in_response
                     inverse_document_frequency = np.log(len(self.corpus) / self.document_frequencies[term])
                     feature_scores[term] = term_frequency * inverse_document_frequency
@@ -48,11 +57,7 @@ if __name__ == '__main__':
         if c_value > th_threshold:
             terms.add(term)
 
-    document_frequencies = {}
-    for term, frequency in th_analyzer.term_frequencies.items():
-        document_frequencies[th_analyzer.get_full_term(term)] = frequency
-
-    analyzer = TfidfAnalyzer(tagger.corpus, terms, document_frequencies)
+    analyzer = TfidfAnalyzer(tagger.corpus, terms)
     result = analyzer.run()
     threshold = 0
     for response, scores in result.items():